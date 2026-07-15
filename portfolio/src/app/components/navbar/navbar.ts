7e482011e0373bcce19c7f24778646b99bf61b40
Santiago Rodriguez <erikrodriguezespitia@gmail.com>
2026-07-15 17:53:42 -0500

    update

--- portfolio/src/app/components/navbar/navbar.ts
+++ portfolio/src/app/components/navbar/navbar.ts
@@ -10,4 +10,9 @@ import { RouterModule, Router } from '@angular/router';
 })
 export class Navbar {
   private translateService = inject(TranslateService);
+
+  onLangToggle(event: Event) {
+    const checked = (event.target as HTMLInputElement).checked;
+    this.translateService.use(checked ? 'es' : 'en');
+  }
 }
